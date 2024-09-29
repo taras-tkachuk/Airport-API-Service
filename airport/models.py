8d2c5b95c4069dea60c1b3ecc7bf77fc49a6817e
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-09-29 23:07:17 +0300

    Create models

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,96 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=69)
+    last_name = models.CharField(max_length=69)
+
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name}"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=69)
+    closet_big_city = models.CharField(max_length=69)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.source} - {self.destination} ({self.distance} km)"
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=69)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=69)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType,
+        on_delete=models.CASCADE,
+        related_name="airplanes"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.airplane_type.name}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        Airplane,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crews = models.ManyToManyField(Crew, related_name="flights")
+
+    def __str__(self) -> str:
+        return f"{self.route} ({self.departure_time} - {self.arrival_time})"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        Flight,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.flight} (row: {self.row}, seat:{self.seat})"
