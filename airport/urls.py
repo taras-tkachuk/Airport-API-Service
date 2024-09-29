38225b955ee8f0957309e23d8d421d42993b7c47
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-09-30 00:02:53 +0300

    Add endpoint for crew

---
+++ airport/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import CrewViewSet
+
+router = routers.DefaultRouter()
+router.register("crews", CrewViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "airport"
--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.serializers import CrewSerializer
+from airport.models import Crew
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
